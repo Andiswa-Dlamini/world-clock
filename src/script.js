28ac962c9f9ba3a59e783c86fa6f17c2c3b921d7
Andiswa-Dlamini <adlamini179@gmail.com>
2025-07-29 11:56:44 +0200

    initial commit

--- src/script.js
+++ src/script.js
@@ -1,38 +1,48 @@
-// console.log(moment.tz.guess());
-// alert(moment.tz.guess());
-
-// const localTime = moment().tz("Africa/Johannesburg").format("YYYY-MM-DD HH:mm");
-// console.log(localTime);
-
-
-const getLocalCity = (event) => {
-    if (event.target.value === "my-local-city"){
+const DefaultCity = () => {
     let localCity = moment.tz.guess();
     let date = moment.tz(localCity).format("DD MMMM YYYY");
-    let time = moment.tz(localCity).format("HH:mm");
-    document.getElementById("city-name").textContent = moment.tz.guess();
-    document.getElementById("city-date").textContent = date;
-    document.getElementById("city-time").textContent = time;
+    let time = moment.tz(localCity).format("HH:mm:ss [<small>]A[</small>]");
+    document.querySelector(".city-name").textContent = moment.tz.guess();
+    document.querySelector(".city-date").textContent = date;
+    document.querySelector(".city-time").textContent = time;
 
-    alert(localCity);
+
+}
+
+const getLocalCity = (event) => { //unnecessary function
+    if (event.target.value === "local-city"){
+    let localCity = moment.tz.guess();
+    let date = moment.tz(localCity).format("DD MMMM YYYY");
+    let time = moment.tz(localCity).format("HH:mm:ss [<small>]A[</small>]");
+    document.querySelector(".city-name").textContent = moment.tz.guess();
+    document.querySelector(".city-date").textContent = date;
+    document.querySelector(".city-time").innerHTML = time;
+
     }
 
 }
 
-document.getElementById("city").addEventListener("change", getLocalCity);
-
-
-
-console.log(moment.tz.guess());
-
 const getCurrentCity = (event) => {
     let currentCity = event.target.value;
     let date = moment.tz(`${currentCity}`).add(`${currentCity}`).format("DD MMMM YYYY");
-    let time = moment.tz(`${currentCity}`).format("HH:mm");
-    document.getElementById("city-name").textContent = currentCity;
-    document.getElementById("city-date").textContent = date;
-    document.getElementById("city-time").textContent = time;
-    }
+    let time = moment.tz(`${currentCity}`).format("HH:mm:ss  [<small>]A[</small>]");
+    document.querySelector(".city-name").textContent = currentCity;
+    document.querySelector(".city-date").textContent = date;
+    document.querySelector(".city-time").innerHTML = time;
+    document.querySelector(".cities").classList.add("hidden");
+    
+}
+
+const displayCities = () => {
+    document.querySelector(".cities").classList.toggle("hidden");
+}
 
 
+DefaultCity();
+document.getElementById("city").addEventListener("change", getLocalCity);
 document.getElementById("city").addEventListener("change", getCurrentCity);
+setInterval(() => {
+     
+    setTimeout(getCurrentCity);
+}, 1000);
+document.getElementById("all-cities").addEventListener("click", displayCities)
